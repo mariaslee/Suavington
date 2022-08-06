98306fa2a8908fd7d4aee4f09953c1690d9dc900
marias_lee84@yahoo.com <marias_lee84@yahoo.com>
2022-08-06 14:31:39 -0500

    made change

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("I'm ready for College Football.");
+Console.WriteLine("I'm ready for the NFL and College Football.");
